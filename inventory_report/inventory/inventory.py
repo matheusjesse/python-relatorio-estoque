49cb0324d05961acfdbfcc6b2636e30ee2049068
matheusjesse <matheus_jesse@hotmail.com>
2022-11-28 06:39:23 -0300

    req05

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,23 @@
+import json
+import csv
+import xmltodict
+
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory:
+    @staticmethod
+    def import_data(path, type):
+        with open(path) as data:
+            if path.endswith(".json"):
+                data = json.load(data)
+            if path.endswith(".csv"):
+                data = list(csv.DictReader(data))
+            if path.endswith(".xml"):
+                data = xmltodict.parse(data.read())["dataset"]["record"]
+
+        if type == "complete":
+            return CompleteReport.generate(data)
+        else:
+            return SimpleReport.generate(data)
