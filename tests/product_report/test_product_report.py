b8baaad040d3e3d0a2b669b0f25019c338319bc2
matheusjesse <matheus_jesse@hotmail.com>
2022-11-28 08:34:36 -0300

    test

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        2,
+        'Maça',
+        'Caxias',
+        '07/03/2021',
+        '07/09/2021',
+        '3CA0',
+        'em ambiente arejado'
+    )
+    message = (
+        f'O produto {product.nome_do_produto}'
+        f' fabricado em {product.data_de_fabricacao}'
+        f' por {product.nome_da_empresa}'
+        f' com validade até {product.data_de_validade}'
+        f' precisa ser armazenado {product.instrucoes_de_armazenamento}.'
+    )
+    assert product.__repr__() == message
